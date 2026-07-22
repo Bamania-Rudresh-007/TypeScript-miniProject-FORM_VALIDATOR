4c10387ca0eb78b930bcb29d2038c86589c330b6
Rudresh <bamaniarudresh2008@gmail.com>
2026-07-22 21:07:08 +0530

    feat: add enums for expense categories and payment methods

---
+++ expense-tracker-cli/src/constants/enums.ts
@@ -0,0 +1,14 @@
+export enum Category {
+  Food = "Food",
+  Travel = "Travel",
+  Shopping = "Shopping",
+  Bills = "Bills",
+  Entertainment = "Entertainment",
+  Others = "Others"
+}
+
+export enum PaymentMethod {
+  Cash = "Cash",
+  UPI = "UPI",
+  Card = "Card"
+}
